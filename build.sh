2646229c9e55046c7d220a864962c773d504fd01
YUVALMARK <yyuval789@gmail.com>
2025-06-24 17:48:53 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-echo "🔥 build.sh is running!"
+echo "🚀 build.sh is running!"
+
+# עדכון pip לגרסה האחרונה
 pip install --upgrade pip
+
+# התקנת כל הספריות מהקובץ
 pip install -r requirements.txt
+
+# בדיקה שההתקנה של moviepy הצליחה
+echo "🔍 Checking moviepy import..."
 python -c "from moviepy.editor import *"
